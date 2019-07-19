a4423193128c1af656f71dc8cefb20b4ca655fd2
Sung Yub Lee <sungyub1399@gmail.com>
2019-07-20 00:33:22 +0900

    see more button added

--- src/components/MainPage/StatInfo/StatInfoBox.js
+++ src/components/MainPage/StatInfo/StatInfoBox.js
@@ -5,8 +5,21 @@ import Sales from "./Sales";
 import TodayVisitors from "./TodayVisitors";
 import RegMembers from "./RegMembers";
 import ChangeMembers from "./ChangeMembers";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
+  width: 100%;
+  padding: 10px;
+  background-color: #f4f6fc;
+  box-shadow: ${props => props.theme.boxShadow};
+  border-bottom: 1px solid #e6e6e6;
+  border-radius: ${props => props.theme.borderRadius};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const LinkContainer = styled.div`
   width: 100%;
   padding: 10px;
   background-color: #f4f6fc;
@@ -20,12 +33,17 @@ const Container = styled.div`
 
 export default () => {
   return (
-    <Container>
-      <Analysis />
-      <TodayVisitors />
-      <RegMembers />
-      <ChangeMembers />
-      <Sales />
-    </Container>
+    <>
+      <Container>
+        <Analysis />
+        <TodayVisitors />
+        <RegMembers />
+        <ChangeMembers />
+        <Sales />
+      </Container>
+      <LinkContainer>
+        <Link to="/stat">더보기</Link>
+      </LinkContainer>
+    </>
   );
 };
